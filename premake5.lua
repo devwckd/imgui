0cf9d5802390c84c4e78396cac38e58898e6e856
devwckd <dev.wckd@gmail.com>
2024-09-03 16:46:54 -0300

    feat: add backend files

--- premake5.lua
+++ premake5.lua
@@ -8,8 +8,8 @@ project "ImGui"
 
 	files
 	{
-	    "backends/imgui_impl_opengl3.h"
-	    "backends/imgui_impl_opengl3.cpp"
+	      "backends/imgui_impl_opengl3.h",
+	    "backends/imgui_impl_opengl3.cpp",
 		"imconfig.h",
 		"imgui.h",
 		"imgui.cpp",
